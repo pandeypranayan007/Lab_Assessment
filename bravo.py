c0474252af139aa5eba60b9f482c3a5e72b5f5ed
pandeypranayan007 <pandeypranayan@gmail.com>
2025-04-23 12:11:26 +0530

    seven commit

---
+++ bravo.py
@@ -0,0 +1,5 @@
+# Using the sum() function with range()
+sum_of_numbers = sum(range(101, 200))
+
+# Printing the result
+print("Sum of all integers greater than 100 and less than 200:", sum_of_numbers)
