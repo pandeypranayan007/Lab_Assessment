6c989fd3df716b71c21a375929c09165e420f8ea
Paranayan Pandey <84219443+pandeypranayan007@users.noreply.github.com>
2025-04-23 10:49:32 +0530

    Create JoinWords.py

---
+++ JoinWords.py
@@ -0,0 +1,11 @@
+# Taking input from the user
+string = input("Enter a string: ")
+
+# Splitting the string into a list of words
+words = string.split()
+
+# Sorting the words alphabetically
+words.sort()
+
+# Printing the sorted words
+print("Sorted words:", " ".join(words))
